ca27008ccb05e3a3efb77ecf4db51eb8a108eae6
SEC\user <dorothy_007@naver.com>
2019-05-28 14:03:33 +0900

    리듀서와 컴포넌트 연결
    리듀서 데이터 가져오기

--- src/A2.js
+++ src/A2.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import A3 from './A3';
 
 class A2 extends Component {
@@ -7,6 +8,7 @@ class A2 extends Component {
       <div>
         <h2>A2</h2>
         <div>{this.props.num}</div>
+        <div>스토어에서 가져온 데이터 : {this.props.storeNum}</div>
 
         <A3 num={this.props.num} />
       </div>
@@ -14,4 +16,11 @@ class A2 extends Component {
   }
 }
 
-export default A2;
+const mapStateToProps = (state) => {
+  return {
+    storeNum : state.control.count
+  }
+}
+
+// connect(데이터관련된것을 가져옴, 액션가져옴)
+export default connect(mapStateToProps,null)(A2);
