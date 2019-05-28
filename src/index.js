3346f4c095a05157983dadee294491bfb2ebf36b
SEC\user <dorothy_007@naver.com>
2019-05-28 13:59:02 +0900

    Redux 연결

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/* Redux 연결 */
+// createStore(데이터 보관 장소)를 Redux에서 가져온다.
+import {createStore} from 'redux';
+
+// React App에 Redux를 적용 할 때에는, react-redux 에 들어있는 Provider(공급자)를 사용
+import {Provider} from 'react-redux';
+
+// Redux파일을 스토어에 보관한 것을 상수 store에 넣기
+import reducers from './reducers/number';
+const store = createStore(reducers);
+
 ReactDOM.render(
-	<App />, document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>, document.getElementById('root')
 );
